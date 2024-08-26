da57c68fbd91e31173307e058d3f836eeba62284
Marius Glittum <marius.glittum@nav.no>
2024-08-26 21:56:24 +0200

    Legger til støtte for map med key type av annet enn string

--- src/main/kotlin/com/papsign/ktor/openapigen/schema/builder/provider/DefaultMapSchemaProvider.kt
+++ src/main/kotlin/com/papsign/ktor/openapigen/schema/builder/provider/DefaultMapSchemaProvider.kt
@@ -20,12 +20,12 @@ object DefaultMapSchemaProvider: SchemaBuilderProviderModule, OpenAPIGenModuleEx
         override val superType: KType = getKType<Map<*, *>?>()
         override fun build(type: KType, builder: FinalSchemaBuilder, finalize: (SchemaModel<*>)->SchemaModel<*>): SchemaModel<*> {
             checkType(type)
-            if (type.arguments[0].type != getKType<String>()) error("bad type $type: Only maps with string keys are supported")
             val valueType = type.arguments[1].type ?: error("bad type $type: star projected types are not supported")
             @Suppress("UNCHECKED_CAST")
             return finalize(SchemaModel.SchemaModelMap(
-                builder.build(valueType) as SchemaModel<Any?>,
-                type.isMarkedNullable
+                additionalProperties = builder.build(valueType) as SchemaModel<Any?>,
+                nullable = type.isMarkedNullable,
+                description = "Key type: ${type.arguments[0].type.toString()}"
             ))
         }
     }
