9d177b90a2117d1882d11dfa1380c276d8f5c1be
Marius Glittum <marius.glittum@nav.no>
2024-09-13 22:20:43 +0200

    Forsøker å henger swagger-ui version fra meta-inf, hvis ikke defaulter til hardkodet verdi. Logger non-hits

--- src/main/kotlin/com/papsign/ktor/openapigen/OpenAPIGen.kt
+++ src/main/kotlin/com/papsign/ktor/openapigen/OpenAPIGen.kt
@@ -65,7 +65,7 @@ class OpenAPIGen(
 
         var swaggerUiPath = "swagger-ui"
         var serveSwaggerUi = true
-        var swaggerUiVersion = SwaggerUIVersion.version // TODO: Fetch from properties or something
+        var swaggerUiVersion = SwaggerUIVersion.version
 
         var scanPackagesForModules: Array<String> = arrayOf()
 
