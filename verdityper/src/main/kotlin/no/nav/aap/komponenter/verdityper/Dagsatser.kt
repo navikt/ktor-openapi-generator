1f1a935bea393095fd351e7a711ed381c5a14d17
yongbinkwon <yong.bin.kwon@nav.no>
2024-11-15 12:55:33 +0100

    Legg til metode for å gange dagsats med heltall
    Co-authored-by: Peter Brottveit Bock <peter.brottveit.bock@nav.no>

--- verdityper/src/main/kotlin/no/nav/aap/komponenter/verdityper/Dagsatser.kt
+++ verdityper/src/main/kotlin/no/nav/aap/komponenter/verdityper/Dagsatser.kt
@@ -3,4 +3,8 @@ package no.nav.aap.komponenter.verdityper
 /** representerer en kvantitet i enhet dagsatser */
 public data class Dagsatser(
     val antall: Int
-)
+) {
+    public operator fun times(other: Int): Dagsatser {
+        return Dagsatser(antall * other)
+    }
+}
