a52acb5a299419cdd28b4c135d0703b4a039bdd0
Fredrik Meyer <fredrik.meyer@nav.no>
2024-12-17 12:51:04 +0100

    Bump Flyway

--- dbtest/build.gradle.kts
+++ dbtest/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     implementation("com.zaxxer:HikariCP:6.2.1")
-    implementation("org.flywaydb:flyway-database-postgresql:11.0.1")
+    implementation("org.flywaydb:flyway-database-postgresql:11.1.0")
     runtimeOnly("org.postgresql:postgresql:42.7.4")
 
     implementation("org.testcontainers:postgresql:1.20.4")
