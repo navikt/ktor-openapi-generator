72938ae69204117009592cc21e1cd8a1b1818b10
yongbinkwon <yong.bin.kwon@nav.no>
2025-01-16 14:43:27 +0100

    sett scale på TimerArbeid.antallTimer, slik at det som kommer inn og ut fra db er likt

--- verdityper/src/main/kotlin/no/nav/aap/komponenter/verdityper/TimerArbeid.kt
+++ verdityper/src/main/kotlin/no/nav/aap/komponenter/verdityper/TimerArbeid.kt
@@ -2,8 +2,24 @@ package no.nav.aap.komponenter.verdityper
 
 import java.math.BigDecimal
 
-public data class TimerArbeid(val antallTimer: BigDecimal) {
+public class TimerArbeid(antallTimer: BigDecimal) {
     init {
         require(antallTimer >= BigDecimal.ZERO) { "Kan ikke jobbe mindre enn 0 timer" }
+    }
+
+    //TODO - holder det med 1 desimal her? Har man lyst til å alltid runde til gunstigst istedet?
+    public val antallTimer: BigDecimal = antallTimer.setScale(1, java.math.RoundingMode.HALF_UP)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as TimerArbeid
+
+        return antallTimer == other.antallTimer
+    }
+
+    override fun hashCode(): Int {
+        return antallTimer.hashCode()
     }
 }
