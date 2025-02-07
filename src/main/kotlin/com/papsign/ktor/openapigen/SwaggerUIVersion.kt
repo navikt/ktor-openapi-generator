510d3ebea30faf6d6c9a906a220214b4a19da750
Marius Bækken Glittum <ekunamatata@gmail.com>
2025-02-07 13:13:22 +0100

    woops, feil versjon

--- src/main/kotlin/com/papsign/ktor/openapigen/SwaggerUIVersion.kt
+++ src/main/kotlin/com/papsign/ktor/openapigen/SwaggerUIVersion.kt
@@ -18,7 +18,7 @@ object SwaggerUIVersion {
             version = properties.getProperty("version")
         } else {
             log.warn("Klarte ikke hente versjon på swagger UI via avhengigheten")
-            version = "5.18.2"
+            version = "5.18.3"
         }
     }
 }
