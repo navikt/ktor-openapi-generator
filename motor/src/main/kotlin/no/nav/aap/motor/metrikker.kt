c3dde539b449753255ab0eca10919be0850b8a60
Fredrik Meyer <fredrik.meyer@nav.no>
2025-02-14 15:10:11 +0100

    Også publiser histogram

--- motor/src/main/kotlin/no/nav/aap/motor/metrikker.kt
+++ motor/src/main/kotlin/no/nav/aap/motor/metrikker.kt
@@ -9,5 +9,8 @@ public fun MeterRegistry.motorFeiletTeller(jobbInput: JobbInput): Counter =
     this.counter("motor_jobb_feilet", listOf(ImmutableTag("type", jobbInput.type())))
 
 public fun MeterRegistry.timer(jobbInput: JobbInput): Timer {
-    return Timer.builder("motor_jobb_timer").tags(listOf(ImmutableTag("type", jobbInput.type()))).register(this);
+    return Timer.builder("motor_jobb_timer")
+        .tags(listOf(ImmutableTag("type", jobbInput.type())))
+        .publishPercentileHistogram()
+        .register(this)
 }
