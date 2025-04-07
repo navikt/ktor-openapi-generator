f16c51b0ac1c36ea19641a3b8f4f9fdbf3e66d82
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-04-07 22:53:23 +0200

    Gjenta where-sjekk i for-update-delen av spørringen

--- motor/src/main/kotlin/no/nav/aap/motor/JobbRepository.kt
+++ motor/src/main/kotlin/no/nav/aap/motor/JobbRepository.kt
@@ -39,7 +39,37 @@ internal class JobbRepository(private val connection: DBConnection) {
     }
 
     internal fun plukkJobb(): JobbInput? {
-
+        /** Selv om `jobb_kandidat` kun inneholder jobber med `status = 'KLAR'`, så er det ikke
+         * noe som forhindrer at flere transaksjoner startet med samme snapshot og derfor
+         * anser samme rad som ledig. `FOR UPDATE` er ikke tilstrekkelig for å forhindre at
+         * vi returnerer en rad som en annen transaksjon har låst. Vi trenger en `WHERE` sjekk
+         * sammen med `FOR UPDATE`, først da vil postgres synkronisere sjekken mot andre transaksjoner.
+         * Fra dokumentasjonen om `READ COMMITED`:
+         *
+	     * > UPDATE, DELETE, SELECT FOR UPDATE, and SELECT FOR
+	     * > SHARE commands behave the same as SELECT in terms of
+	     * > searching for target rows: they will only find target
+	     * > rows that were committed as of the command start time.
+	     * > However, such a target row might have already been
+	     * > updated (or deleted or locked) by another concurrent
+	     * > transaction by the time it is found. In this case, the
+	     * > would-be updater will wait for the first updating
+	     * > transaction to commit or roll back (if it is still in
+	     * > progress). If the first updater rolls back, then its
+	     * > effects are negated and the second updater can proceed
+	     * > with updating the originally found row. If the first
+	     * > updater commits, the second updater will ignore the row
+	     * > if the first updater deleted it, otherwise it will
+	     * > attempt to apply its operation to the updated version
+	     * > of the row. The search condition of the command (the
+	     * > WHERE clause) is re-evaluated to see if the updated
+	     * > version of the row still matches the search condition.
+	     * > If so, the second updater proceeds with its operation
+	     * > using the updated version of the row. In the case of
+	     * > SELECT FOR UPDATE and SELECT FOR SHARE, this means it
+	     * > is the updated version of the row that is locked and
+	     * > returned to the client.
+         */
         val query = """
             with ekskluderende_jobb as (
                 select distinct on (sak_id, behandling_id) id, status, neste_kjoring
@@ -82,6 +112,7 @@ internal class JobbRepository(private val connection: DBConnection) {
                       and jobb_historikk.status = '${JobbStatus.FEILET.name}') as antall_feil
             from jobb
             inner join jobb_kandidat on jobb_kandidat.id = jobb.id
+            where jobb.status = '${JobbStatus.KLAR.name}'
             order by jobb_kandidat.neste_kjoring asc
             for update skip locked
             limit 1
