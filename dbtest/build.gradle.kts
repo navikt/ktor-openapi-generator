09ae3c187ba7c5b9693233ec8ab974955786e1e9
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-08-15 14:05:31 +0200

    Extension-versjon av database som lukker connection pool
    Databasen kan nå grensen for åpne connections. Bruker man
    denne extensionen, så forhindrer man det.
    
    co-authored-by: Ine Lofthus Arnesen <Ine.Lofthus.Arnesen@nav.no>

--- dbtest/build.gradle.kts
+++ dbtest/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     implementation("com.zaxxer:HikariCP:7.0.1")
     implementation("org.flywaydb:flyway-database-postgresql:11.11.1")
     runtimeOnly("org.postgresql:postgresql:42.7.7")
+    api("org.junit.jupiter:junit-jupiter-api:5.13.1")
 
     implementation("org.testcontainers:postgresql:1.21.3")
     constraints {
---
+++ dbtest/src/main/kotlin/no/nav/aap/komponenter/dbtest/TestDatabase.kt
@@ -0,0 +1,34 @@
+package no.nav.aap.komponenter.dbtest
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.api.extension.TestInstancePostProcessor
+import org.junit.jupiter.api.extension.TestInstancePreDestroyCallback
+import org.junit.platform.commons.support.AnnotationSupport.findAnnotatedFields
+import java.io.Closeable
+import java.lang.reflect.Field
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+@ExtendWith(TestDatabaseExtension::class)
+public annotation class TestDatabase
+
+public class TestDatabaseExtension : TestInstancePreDestroyCallback, TestInstancePostProcessor {
+    override fun postProcessTestInstance(testInstance: Any?, context: ExtensionContext?) {
+        findAnnotatedFields(context!!.requiredTestClass, TestDatabase::class.java)
+            .forEach<Field> { field ->
+                field.setAccessible(true)
+                field.set(testInstance, InitTestDatabase.freshDatabase())
+            }
+    }
+
+    override fun preDestroyTestInstance(context: ExtensionContext?) {
+        TestInstancePreDestroyCallback.preDestroyTestInstances(context) { testInstance ->
+            findAnnotatedFields(context!!.requiredTestClass, TestDatabase::class.java)
+                .forEach<Field> {
+                    (it.get(testInstance) as Closeable).close()
+                }
+        }
+    }
+}
+
