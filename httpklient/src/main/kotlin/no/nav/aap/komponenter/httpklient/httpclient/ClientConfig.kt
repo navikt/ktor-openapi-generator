b7542ae4f191f459630d1f2d8a2d50b6102aff57
Charlie Midtlyng <charlie.midtlyng@bekk.no>
2025-08-21 12:59:41 +0200

    Bør ikke ha for lang connection-timeout - det kan gi risiko for at man spiser og holder på for mange connections for lenge, helt unødvendig. Ingen av våre normale kall bør bruke mer enn noen hundre millisekunder på å opprette en connection (#231)

--- httpklient/src/main/kotlin/no/nav/aap/komponenter/httpklient/httpclient/ClientConfig.kt
+++ httpklient/src/main/kotlin/no/nav/aap/komponenter/httpklient/httpclient/ClientConfig.kt
@@ -5,7 +5,7 @@ import java.time.Duration
 
 public class ClientConfig(
     internal val scope: String? = null,
-    internal val connectionTimeout: Duration = Duration.ofSeconds(15),
+    internal val connectionTimeout: Duration = Duration.ofSeconds(2),
     internal val additionalHeaders: List<Header> = emptyList(),
     internal val additionalFunctionalHeaders: List<FunctionalHeader> = emptyList()
 )
