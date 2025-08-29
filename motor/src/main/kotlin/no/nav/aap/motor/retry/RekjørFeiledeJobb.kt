e4505034f3c1787160bae48cebe30784a8fb8af1
Charlie Midtlyng <charlie.midtlyng@bekk.no>
2025-08-29 15:49:19 +0200

    Retry feilede jobber sjeldnere (#237)
    * Når vi rekjører feilede jobber hvert kvarter bruker vi unødig mye kapasitet til å reprosessere samme feil om igjen. Ved å kjøre sjeldnere vil det trolig oppleves mer stabilt for saksbehandlere
    
    * Legg inn kommentar som beskriver cron-uttrykket

--- motor/src/main/kotlin/no/nav/aap/motor/retry/RekjørFeiledeJobb.kt
+++ motor/src/main/kotlin/no/nav/aap/motor/retry/RekjørFeiledeJobb.kt
@@ -30,7 +30,10 @@ internal class RekjørFeiledeJobb(private val repository: RetryFeiledeJobberRepo
         override val beskrivelse =
             "Finner feilende jobber og markerer disse som klar for nytt forsøk ved hver kjøring av denne jobben."
 
-        override val cron = CronExpression.create("0 0,15,30,45 7-17,20 * * *")
+        /**
+         * Hver dag kl 07:00, 12:00, 15:00 og 20:00
+         */
+        override val cron = CronExpression.create("0 0 7,12,15,20 * * *")
 
     }
 }
