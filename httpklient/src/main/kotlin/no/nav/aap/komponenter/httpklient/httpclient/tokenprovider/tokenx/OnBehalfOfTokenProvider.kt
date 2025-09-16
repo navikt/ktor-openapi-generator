edbdf6bbb46c6f1534884ed87624f65f6683a180
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2025-09-16 09:58:54 +0200

    Behold abi-kompabilitet for deprecated klasse

--- httpklient/src/main/kotlin/no/nav/aap/komponenter/httpklient/httpclient/tokenprovider/tokenx/OnBehalfOfTokenProvider.kt
+++ httpklient/src/main/kotlin/no/nav/aap/komponenter/httpklient/httpclient/tokenprovider/tokenx/OnBehalfOfTokenProvider.kt
@@ -1,4 +1,18 @@
 package no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.tokenx
 
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry
+import no.nav.aap.komponenter.config.requiredConfigForKey
+import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.TexasTokenProvider
+import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.TokenProvider
+import java.net.URI
+
 @Deprecated("Brukt TokenxOBOTokenProvider")
-public typealias OnBehalfOfTokenProvider = TokenxOBOTokenProvider
+public class OnBehalfOfTokenProvider(
+    texasUri: URI = URI(requiredConfigForKey("nais.token.exchange.endpoint")),
+    prometheus: MeterRegistry = SimpleMeterRegistry(),
+) : TokenProvider by TexasTokenProvider(
+    texasUri = texasUri,
+    identityProvider = "tokenx",
+    prometheus = prometheus,
+)
