17bbcea21e375de608bf78abd94df7d9398bdab9
charliemidtlyng <charlie.midtlyng@bekk.no>
2025-09-24 11:59:12 +0200

    Degradere postgresql driver

--- dbmigrering/build.gradle.kts
+++ dbmigrering/build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 dependencies {
     implementation(project(":infrastructure"))
     implementation("org.flywaydb:flyway-database-postgresql:11.13.1")
-    runtimeOnly("org.postgresql:postgresql:42.7.8")
+    runtimeOnly("org.postgresql:postgresql:42.7.7")
 }
