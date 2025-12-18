4adca7eed658856f59dc48e61e5b0863f3a97689
Nicolas N. <1224956+Watercolours@users.noreply.github.com>
2025-12-18 10:16:35 +0100

    tilgjengeliggjøre ident på token (#325)
    * tilgjengeliggjøre ident på token
    
    * to ulike funksjoner per claim, i stedet for én
    
    * ikke eksponere azpName på token-nivå
    
    * bedre feilhåndtering

--- httpklient/src/main/kotlin/no/nav/aap/komponenter/httpklient/httpclient/tokenprovider/OidcToken.kt
+++ httpklient/src/main/kotlin/no/nav/aap/komponenter/httpklient/httpclient/tokenprovider/OidcToken.kt
@@ -4,9 +4,14 @@ import com.auth0.jwt.JWT
 import java.time.LocalDateTime
 import java.time.ZoneId
 
+/**
+ * Mer info om tilgjengelige claims i token:
+ * https://docs.nais.io/auth/entra-id/reference/#claims
+ **/
 private const val OID = "oid"
 private const val IDTYP = "idtyp"
 private const val APP = "app"
+private const val NAVident = "NAVident"
 
 public class OidcToken(accessToken: String) {
     private val accessToken = JWT.decode(accessToken)
@@ -24,11 +29,25 @@ public class OidcToken(accessToken: String) {
         return now.isBefore(expires())
     }
 
+    /**
+     * Sjekker om token er et systembruker-token (client credentials)
+     **/
     public fun isClientCredentials(): Boolean {
         val subject = accessToken.subject
 
         // Sjekker både gammel konvensjon (oid=sub) og nyere (idtyp="app")
         return subject == accessToken.getClaim(OID).asString() ||
                 APP == accessToken.getClaim(IDTYP).asString()
+    }
+
+    /**
+     * Returnerer NAVident (Z-ident) for personbruker-token
+     *
+     * @throws IllegalStateException hvis token tilhører systembruker
+     **/
+    public fun navIdent(): String {
+        check(!isClientCredentials()) { "Kan kun hente NAVident for personbruker" }
+
+        return accessToken.getClaim(NAVident).asString()
     }
 }
